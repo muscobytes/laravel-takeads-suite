aa411b029c87b1a8c690566b49af8a48587319d7
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-30 02:00:00 +0400

    test(models): add test for `TakeadsCoupon` model and category-coupon relationship
    - Add `TakeadsCouponTest.php` to test the creation of `TakeadsCoupon` model
    - Add `test_category_coupon_relation` in `TakeadsCategoryTest.php` to verify the category-coupon relationship
    - Ensure the correct attributes and relationships are asserted in the tests

--- tests/Unit/Models/TakeadsCategoryTest.php
+++ tests/Unit/Models/TakeadsCategoryTest.php
@@ -3,6 +3,7 @@
 namespace Muscobytes\Laravel\Takeads\Suite\Tests\Unit\Models;
 
 use Muscobytes\Laravel\Takeads\Suite\Models\TakeadsCategory;
+use Muscobytes\Laravel\Takeads\Suite\Models\TakeadsCoupon;
 use Muscobytes\Laravel\Takeads\Suite\Tests\TestCase;
 use PHPUnit\Framework\Attributes\CoversClass;
 
@@ -19,6 +20,34 @@ class TakeadsCategoryTest extends TestCase
         $this->assertIsInt($category->external_id);
         $this->assertSame($category->external_id, $external_id);
         $this->assertEquals(1, TakeadsCategory::count());
+    }
 
+
+    public function test_category_coupon_relation()
+    {
+        $category = TakeadsCategory::create([
+            'external_id' => 43
+        ]);
+        $this->assertEquals($category->coupons()->count(), 0);
+
+        $coupon = TakeadsCoupon::create([
+            'external_id' => '0103Gkr5QEPahe6calErk',
+            'is_active' => true,
+            'tracking_link' => 'https://tatrck.com/h/1Hu30zX',
+            'name' => 'Tendance Cadeau - -5€ sur toute la boutique à partir de 25€ d’achats',
+            'code' => 'EFF5E',
+            'merchant_id' => 1,
+            'image_uri' => 'https://monetizeamex.com/logos_v2/90x45/7878f4bbd60eea86eee0afd69669aaa8.gif',
+            'start_date' => '2023-03-08T23:00:00.000Z',
+            'end_date' => '2030-01-01T00:00:00.000Z',
+            'description' => 'descriptino'
+        ]);
+
+        $category->coupons()->attach($coupon);
+
+        $this->assertEquals($category->coupons()->count(), 1);
+        $this->assertEquals($category->coupons()->where('is_active', true)->count(), 1);
+        $this->assertSame($category->coupons()->first()->external_id, '0103Gkr5QEPahe6calErk');
+        $this->assertSame($category->coupons()->first()->code, 'EFF5E');
     }
 }
---
+++ tests/Unit/Models/TakeadsCouponTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Muscobytes\Laravel\Takeads\Suite\Tests\Unit\Models;
+
+use DateTimeInterface;
+use Muscobytes\Laravel\Takeads\Suite\Models\TakeadsCoupon;
+use Muscobytes\Laravel\Takeads\Suite\Tests\TestCase;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(TakeadsCoupon::class)]
+class TakeadsCouponTest extends TestCase
+{
+    public function test_coupon_model_creation()
+    {
+        $coupon = TakeadsCoupon::create([
+            'external_id' => '003vzm1sl12i4vxnJkk7O',
+            'is_active' => true,
+            'tracking_link' => 'https://tatrck.com/h/1Hu',
+            'name' => '5€ sur toute la boutique à partir de 25€ d’achats',
+            'code' => 'EFF5E',
+            'merchant_id' => 202544,
+            'image_uri' => 'https://monetizeamex.com/123',
+            'start_date' => '2023-03-08T23:00:00.000Z',
+            'end_date' => '2030-01-01T00:00:00.000Z',
+            'description' => 'Description and other conditions'
+        ]);
+
+        $this->assertInstanceOf(TakeadsCoupon::class, $coupon);
+
+        $this->assertIsString($coupon->external_id);
+        $this->assertEquals('003vzm1sl12i4vxnJkk7O', $coupon->external_id);
+
+        $this->assertIsBool($coupon->is_active);
+        $this->assertTrue($coupon->is_active);
+
+        $this->assertIsString($coupon->tracking_link);
+        $this->assertSame('https://tatrck.com/h/1Hu', $coupon->tracking_link);
+
+        $this->assertIsString($coupon->name);
+        $this->assertSame('5€ sur toute la boutique à partir de 25€ d’achats', $coupon->name);
+
+        $this->assertIsInt($coupon->merchant_id);
+        $this->assertSame(202544, $coupon->merchant_id);
+
+        $this->assertIsString($coupon->image_uri);
+        $this->assertSame('https://monetizeamex.com/123', $coupon->image_uri);
+
+        $this->assertInstanceOf(DateTimeInterface::class, $coupon->start_date);
+        $this->assertSame('2023-03-08 23:00:00', $coupon->start_date->format('Y-m-d H:i:s'));
+
+        $this->assertInstanceOf(DateTimeInterface::class, $coupon->end_date);
+        $this->assertSame('2030-01-01 00:00:00', $coupon->end_date->format('Y-m-d H:i:s'));
+    }
+}
