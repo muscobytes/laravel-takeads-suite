1b3e7ab83073c2d7a4fc9e6e69f4ad1e39d690b8
Stanislav Surskiy <ssurskiy@gmail.com>
2024-10-31 01:36:34 +0400

    refactor(service-provider): simplify migration publishing logic
    - Remove custom `registerMigrations` method in favor of using `PublishesMigrations` trait
    - Enhance code readability and maintainability by leveraging the existing trait functionality

--- src/Providers/TakeadsSuiteServiceProvider.php
+++ src/Providers/TakeadsSuiteServiceProvider.php
@@ -2,38 +2,15 @@
 
 namespace Muscobytes\Laravel\Takeads\Suite\Providers;
 
-use Generator;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Muscobytes\Laravel\Takeads\Suite\Console\Commands\ImportMerchants;
 use Muscobytes\Laravel\Takeads\Suite\Console\Commands\SearchCoupons;
+use Muscobytes\Laravel\TraitsCollection\ServiceProviders\PublishesMigrations;
 
 class TakeadsSuiteServiceProvider extends ServiceProvider
 {
-    /**
-     * Searches migrations and publishes them as assets.
-     * @see https://darkghosthunter.medium.com/laravel-packages-load-or-publish-migrations-119db770c870
-     *
-     * @param string $directory
-     * @return void
-     * @throws BindingResolutionException
-     */
-    protected function registerMigrations(string $directory): void
-    {
-        if ($this->app->runningInConsole()) {
-            $generator = function(string $directory): Generator {
-                foreach ($this->app->make('files')->allFiles($directory) as $file) {
-                    yield $file->getPathname() => $this->app->databasePath(
-                        'migrations/' . now()->format('Y_m_d_His') . Str::after($file->getFilename(), '00_00_00_000000')
-                    );
-                }
-            };
-
-            $this->publishes(iterator_to_array($generator($directory)), 'migrations');
-        }
-    }
-
+    use PublishesMigrations;
 
     /**
      * Bootstrap any package services.
